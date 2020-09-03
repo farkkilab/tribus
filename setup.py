b9cf277c18426f1e1bf8786a9619fa0055a21d67
Casado <casado@ad.helsinki.fi>
2020-09-03 15:57:12 +0300

    setup changes to introduce napari plugin option

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="tribus-jcasado", # Replace with your own username
     version="0.0.1",
-    author="Julia Casado",
+    author="Julia Casado, Angela Szabo, Miikka Kilkkila",
     author_email="julia.casado@helsinki.fi",
     description="Cell type based analysis of multiplexed imaging data",
     long_description=long_description,
@@ -18,5 +18,15 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        "napari.plugin": [
+            "segmentation_qc = tribus.segmentation_qc",
+            "quantification_qc = tribus.quantification_qc",
+            "labels_qc = tribus.labels_qc",
+        ],
+        "console_scripts": [
+            'tribus = tribus.module:main',
+        ],
+    },
     python_requires='>=3.6',
 )
