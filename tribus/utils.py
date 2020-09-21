387a1edd5f9f9d3d5cfc56a9444155727194595b
raviola-byte <60780662+raviola-byte@users.noreply.github.com>
2020-09-21 17:17:05 +0300

    Update utils.py

--- tribus/utils.py
+++ tribus/utils.py
@@ -1,7 +1,9 @@
 '''calls from main go here'''
+import napari_for_tribus
 
 def launch_napari():
     ''' call Angela's functions '''
+    napari_for_tribus.open_napari()
 
 def set_labels():
     ''' launch cell type caller '''
