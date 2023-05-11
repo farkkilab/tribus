Add normalization to tribus, few bug fixes

tribus/visualization.py
@@ -9,6 +9,7 @@ from umap import UMAP
 import math
 import matplotlib.backends.backend_pdf
 from matplotlib.patches import Patch
+import colorcet as cc
 
 palette = sns.dark_palette("#FF0000", as_cmap=True)
 matplotlib.cm.register_cmap("mycolormap", palette)
@@ -79,16 +80,17 @@ def heatmap_for_median_expression(sample_file, labels, logic, level="Global", sa
     filtered_sample = filtered_sample[labels[level].notnull()]
     filtered_labels = labels[level][labels[level].notnull()]
     description_table = logic[level]
-
+    values = [-1, 0, 1]
     table, cell_types = get_subsets(filtered_sample, filtered_labels)
 
     for i in range(len(cell_types)):
         df_median[cell_types[i]] = table[i].iloc[:,:-1].median()
         new_value = description_table[cell_types[i]]
         palette_ = c_palette
-        lut = dict(zip(np.unique(new_value), palette_))
+        lut = dict(zip(values, palette_))
         row_colors = new_value.map(lut)
         df_annotation_table[cell_types[i]] = list(row_colors)
+
 
     df_annotation_table = df_annotation_table.set_index(df_median.index)
     df_median = transform(df_median.transpose())
@@ -105,9 +107,8 @@ def heatmap_for_median_expression(sample_file, labels, logic, level="Global", sa
     return df_median
 
 
-def umap_vis(sample_file, labels, markers, save=False, fname=None,  level="Global", title=None, init='spectral',
-             random_state=0, n_neighbors=10, min_dist=0.1, metric='correlation', palette_markers='mycolormap',
-             palette_cell='tab10', dpi='figure'):
+def umap_vis(sample_file, labels, markers, save = False, fname = None,  level = "Global", title = None, init='spectral',
+             random_state=0, n_neighbors=10, min_dist=0.1, metric='correlation', palette_markers='mycolormap', dpi='figure'):
 
     if type(markers) is dict:
         markers = list(markers[level].index)
@@ -117,7 +118,6 @@ def umap_vis(sample_file, labels, markers, save=False, fname=None,  level="Globa
 
     filtered_labels = labels[level][labels[level].notnull()]
     sample_file_filtered = sample_file[labels[level].notnull()]
-
 
     sample_file_filtered = sample_file_filtered[markers]
     cell_types = np.unique(filtered_labels)
@@ -133,19 +133,19 @@ def umap_vis(sample_file, labels, markers, save=False, fname=None,  level="Globa
     fig.suptitle(title, fontsize=30)
 
     for i in range(len(markers)):
+        print(markers[i])
         if markers[i] == 'labels':
             nr_of_colors = len(cell_types)
-            palette_ = sns.color_palette(palette_cell, nr_of_colors)
+            palette = sns.color_palette(cc.glasbey, n_colors=nr_of_colors)
             proj_2d[markers[i]] = table[markers[i]]
             sns.scatterplot(data=proj_2d, x="component 1", y="component 2", ax=ax[int(i / 3)][i % 3], alpha=0.8,
-                            hue=markers[i], palette=palette_)
+                            hue=markers[i], palette=palette)
         else:
             proj_2d[markers[i]] = table[markers[i]]
-
             sns.scatterplot(data=proj_2d, x="component 1", y="component 2", ax=ax[int(i / 3)][i % 3], alpha=0.8,
                             hue=markers[i], palette=palette_markers)
     if save:
-        plt.save(fname, dpi=dpi)
+        plt.savefig(fname, dpi=dpi)
     else:
         plt.show()
 